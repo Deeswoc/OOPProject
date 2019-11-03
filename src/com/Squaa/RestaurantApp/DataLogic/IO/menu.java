5e6d009b0fdd86b3ec081f81e21b61447eb1fd12
barbi <barbi@DESKTOP-J1EAJ4E>
2019-11-03 15:55:08 -0500

    no change

--- src/com/Squaa/RestaurantApp/DataLogic/IO/menu.java
+++ src/com/Squaa/RestaurantApp/DataLogic/IO/menu.java
@@ -43,14 +43,9 @@ public class menu {
 	{
 		database data  = new database();
     	 ResultSet rs;
-    	 /*try {
 			data.addDish(1, "margarita", 5, 2, 500.00);
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-    	 data.updateDish(1,"pizza", 2, 10, 500.00);
+		
+    	 //data.updateDish(1,"pizza", 2, 10, 500.00);
     	 
     	 try {
     		rs = data.displayUsers();  
@@ -63,7 +58,7 @@ public class menu {
     		e.printStackTrace();
     	} catch (SQLException e) {
     		e.printStackTrace();
-    	}*/
+    	}
 	}
 	
 	
