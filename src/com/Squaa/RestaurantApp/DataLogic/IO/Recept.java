34b3d91951a2505c8f29ed006d71d35c6fd3f7be
barbi <barbi@192.168.1.7>
2019-11-16 17:48:24 -0500

    added back write to file function

---
+++ src/com/Squaa/RestaurantApp/DataLogic/IO/Recept.java
@@ -0,0 +1,68 @@
+package com.Squaa.RestaurantApp.DataLogic.IO;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.filechooser.FileSystemView;
+
+import com.Squaa.RestaurantApp.DataLogic.Order;
+
+public class Recept {
+	//FOR CREATING TXT FILES FOR EACH ORDER CONTAINING ORDER_NUM,DATE,TIME
+	static String orderPath = FileSystemView.getFileSystemView().getDefaultDirectory().getPath()+ "\\Order";
+	public Order order;
+	
+	 public Recept( Order order) {
+			this.order = order;
+		}
+public void writeFile(Order order)
+	{
+		File file = new File(orderPath+"//"+"Order# "+order.getOrderNumber()+".txt");// Let me know if it work on your machine
+		FileWriter fr = null;
+			try {
+		         	fr = new FileWriter(file);
+		            fr.write("Order #: "+order.getOrderNumber()+"\n"
+		                		+"Oder Date: "+order.getDate()+"\n"
+		                		+"Order Time: "+order.getTime());
+		        }
+			catch (IOException e)
+			{
+				e.printStackTrace();
+		    }
+		    	try {
+		             	fr.close();
+		            } 
+		    	catch (IOException e) 
+		    	{
+		    		e.printStackTrace();
+		        }
+		            
+	}
+		
+		public void CreateFileDirectory() 
+		 {
+			 boolean dirFlag  = false;
+			File stockDir = new File(orderPath);
+			
+			try
+			{
+				dirFlag = stockDir.mkdir();
+			}
+			catch(SecurityException Se)
+			{
+				System.out.println("Error marking directory"+ Se);
+			}
+			if(dirFlag)
+			{
+				System.out.println("Directory created Successfully");
+			}
+			else
+			{
+				System.out.println("Directory already exists");
+			}
+		 }
+	}
+
+		
+
