8c9e08fa70260d5eb2e789b235f9114b3fdb5796
Deeswoc <daniel.lor.nelson@gmail.com>
2019-11-20 22:11:23 -0500

    Added interface for OrderController and implemented methods

--- src/com/Squaa/RestaurantApp/Controllers/AddOrderItemController.java
+++ src/com/Squaa/RestaurantApp/Controllers/AddOrderItemController.java
@@ -2,4 +2,33 @@ package com.Squaa.RestaurantApp.Controllers;
 
 public class AddOrderItemController implements AbstractOrderController{
 
+	@Override
+	public void startOrder() {
+
+	}
+
+	@Override
+	public void increaseQuantity(int orderItemID) {
+
+	}
+
+	@Override
+	public void decreaseQuantity(int orderItemID) {
+
+	}
+
+	@Override
+	public void setOrderQuantity(int orderItemID, int orderItemQuantity) {
+
+	}
+
+	@Override
+	public void addOrderItem(int menuItemID) {
+
+	}
+
+	@Override
+	public void removeOrderItem(int orderItemID) {
+
+	}
 }
